55f5967e6c536223ad76a6d4a71048d6fea600ea
fuuApp <ogahirogk1@gmail.com>
2026-05-27 09:44:28 +0900

    fix: unstable_noStore to fully disable Vercel edge cache on login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,11 +2,10 @@
 export const dynamic = 'force-dynamic'
 export const revalidate = 0
 
-import { headers } from 'next/headers'
+import { unstable_noStore as noStore } from 'next/cache'
 import LoginClient from './LoginClient'
 
 export default function Page() {
-  // headers() を呼ぶことで Next.js が必ず動的レンダリングする
-  headers()
+  noStore() // キャッシュを完全に無効化
   return <LoginClient />
 }
